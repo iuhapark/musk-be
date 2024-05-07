fe38dc5cbec560c46e6c0c50b53f0918f11f2f11
GiselePark <hanied04@gmail.com>
2024-05-07 17:35:10 +0900

    feat - update Query

--- src/main/java/com/musk/user/user/repository/UserRepository.java
+++ src/main/java/com/musk/user/user/repository/UserRepository.java
@@ -15,11 +15,12 @@ public interface UserRepository extends JpaRepository<UserEntity, Long> {
     List<UserEntity> findAllByOrderByIdDesc();
 
     Optional<UserEntity> findByUsername(String username);
-    Boolean existByUsername(String username);
 
     @Modifying
     @Query("update users a set a.token = :token where a.id = :id")
     void modifyTokenById(@Param("id") Long id, @Param("token") String token);
 
 
+    @Query("select count(id) as count from users where username =:username")
+    Integer existsByUsername(@Param("username") String username);
 }
